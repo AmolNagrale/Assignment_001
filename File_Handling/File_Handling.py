ea92dbff40c4b12d890befb452cf67e97916d4b9
Amol Nagrale <amolmnagrale1@gmail.com>
2021-07-06 16:30:28 +0530

    File handling programs added

---
+++ File_Handling/File_Handling.py
@@ -0,0 +1,67 @@
+# f = open("abc.txt",'w')
+# f.write("Durga\n")
+# f.write("Software\n")
+# f.write("Solutions\n")
+# print("Data write to the file successfully")
+# f.close()
+
+#==================================================
+
+# #properties of writting a file
+# 
+# f = open("abcd.txt",'w')
+# print("File Name:",f.name)
+# print("File mode",f.mode)
+# print("Is file readable ",f.readable())
+# print("Is file writable ",f.writable())
+# print("Is file closed ",f.closed)
+# f.close()
+# print("Is file closed ",f.closed)
+# #
+
+#properties of reading a file
+
+# f = open("abcdefg.txt",'r')
+# print("File Name:",f.name)
+# print("File mode",f.mode)
+# print("Is file readable ",f.readable())
+# print("Is file writable ",f.writable())
+# print("Is file closed ",f.closed)
+# f.close()
+# print("Is file closed ",f.closed)
+
+
+# #properties of append a file
+# 
+# f = open("abcdefgh.txt",'a')
+# print("File Name:",f.name)
+# print("File mode",f.mode)
+# print("Is file readable ",f.readable())
+# print("Is file writable ",f.writable())
+# print("Is file closed ",f.closed)
+# f.close()
+# print("Is file closed ",f.closed)
+
+#properties of Exclusive 'x' a file
+
+# f = open("abcdefghi.txt",'x')
+# print("File Name:",f.name)
+# print("File mode",f.mode)
+# print("Is file readable ",f.readable())
+# print("Is file writable ",f.writable())
+# print("Is file closed ",f.closed)
+# f.close()
+# print("Is file closed ",f.closed)
+
+#=========================================
+
+# Writing data to text files
+
+# f=open('abcd.txt','w')
+# f.write('Durga\n')
+# f.write('software\n')
+# f.write('solutions\n')
+# print('Data written into file successfully')
+
+#==============================================
+
