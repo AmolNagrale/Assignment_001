364c5e6f519d275fcec8a9a326e04bdda7a6a59a
Amol Nagrale <amolmnagrale1@gmail.com>
2021-07-08 14:15:17 +0530

    revision file added

---
+++ revision_practice.py
@@ -0,0 +1,80 @@
+# fruits = ['mango','cherry','banana']
+# x,y,z = fruits
+# print(x)
+# print(y)
+# print(z)
+# 
+# fruits = ('mango','cherry','banana')
+# x,y,z = fruits
+# print(x)
+# print(y)
+# print(z)
+
+# print(bin(15))
+
+# complex data types
+# 
+# a +bj
+
+# b = True
+# print(type(b))
+
+# a = 10
+# b = 2
+# 
+# 10>20>30
+
+# x = 0
+# while True:
+#     x+=1
+#     print("The programmer",x)
+#    
+
+# for i in range(4):
+#     for j in range(4):
+#         print("i=",i,"j=",j)
+
+# n = int(input("Enter the number of rows: "))
+# for i in range(1, n+1):
+#     print("  *  "*i)
+#
+
+# n = int(input("Enter number of rows :"))
+# for i in range(1, n+1):
+#     for j in range(1, i+1):
+#         print(" *",end=" ")
+#     print()
+
+# n = int(input("Enter number of rows:"))
+# for i in range(1,n+1):
+#     print(" "* (n-i),end="")
+#     print("* "*i)
+
+# cart = [10,20,600,70,60]
+# for item in cart:
+#         if item>500:
+#             print("To place this order insurance must required")
+#             break
+#         print(item)
+
+# for i in range(10):
+#     if i%2==0:
+#         continue
+#     print(i)
+
+# to print numbers from 1 to 10
+
+# n = 1
+# while n<=10:
+#     print(n)
+#     n+=1
+
+
+
+
+
+
+
+
+
+
